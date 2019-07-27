4c300013345d28e86c61c0535eb977dfbf5664a4
jmfdev <53137029+jmfdev@users.noreply.github.com>
2019-07-27 15:50:22 +1000

    Update app.R

--- app.R
+++ app.R
@@ -1,54 +1,159 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#LATEST!
+
 
 library(shiny)
+library(shinyjs)
+library(plotly)
+library(ggplot2)
+library(RColorBrewer)
+library(lubridate)
+
+newData <- function() {
+  rbind(base,data)
+}
+
+# add an asterisk to an input label
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+
+appCSS <-
+  ".mandatory_star { color: red; }
+.shiny-input-container { margin-top: 25px; }
+#submit_msg { margin-left: 15px; }
+#error { color: red; }
+body { background: #fcfcfc; }
+#header { background: #fff; border-bottom: 1px solid #ddd; margin: -20px -15px 0; padding: 15px 15px 10px; }
+"
+
+#setwd("~/Desktop/shinyFormLatest/data")
+#setwd("/srv/shiny-server/appdata")
+
 
 # Define UI for application that draws a histogram
-ui <- pageWithSidebar(
-  headerPanel('Iris k-means clustering'),
-  sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris),
-                selected=names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3,
-                 min = 1, max = 9)
-  ),
-  mainPanel(
-    plotOutput('plot1')
+ui <- fluidPage(
+  shinyjs::useShinyjs(),
+  shinyjs::inlineCSS(appCSS),
+  # Application title
+  titlePanel("Reenoel Restaurant Ratings"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      tabsetPanel(
+        tabPanel("Chart controls", div(
+          id = "form2",
+          selectInput("metric", "Choose Reenoel metric",
+                      choices= c('Score','Food','Atmosphere','Character','Service')),
+          selectInput("color_change", "Colour by:",
+                      choices= c('Restaurant','Meal','Month'))
+        )
+        ),
+        tabPanel("Data Input", div(
+          id = "form",
+          textInput("Restaurant", labelMandatory("Restaurant"), ""),
+          dateInput("Date", label = "Date Visited", value = Sys.Date(), format = "dd MM yyyy"),
+          selectInput("Meal", "Time of Day", c("",  "Breakfast", "Lunch", "Dinner")),
+          sliderInput("Food", "Food", min = 0, max = 10, value = 5, step = 0.5),
+          sliderInput("Atmosphere", "Atmosphere", min = 0, max = 10, value = 5, step = 0.5),
+          sliderInput("Character", "Character", min = 0, max = 10, value = 5, step = 0.5),
+          sliderInput("Service", "Service", min = 0, max = 10, value = 5, step = 0.5),
+          actionButton("submit", "Submit", class = "btn-primary")
+        ))
+      )),
+    # Show a plot of the generated distribution
+    mainPanel(
+    tabsetPanel(
+      tabPanel("Top scorers",
+               h3(textOutput("selected_var")),
+               plotlyOutput("barPlot")
+      ),
+      
+      tabPanel("Data Table", tableOutput('summary'))
+    )
   )
+)
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output, session) {
-  
-  # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
+server <- function(input, output) {
+  #output$table <- renderTable(restaurants, colnames = TRUE)
+  data <- eventReactive(c(input$agent,input$submit), {        
+    rbind( data.frame(read.csv("/home/ubuntu/appdata/restaurants.csv",header=FALSE))
+           ,cbind(input$Restaurant, format(input$Date, "%d/%m/%Y"), input$Meal, input$Food, input$Atmosphere, input$Character, input$Service
+                  , (input$Food * 0.5), (input$Atmosphere * 0.2), (input$Character * 0.2), (input$Service * 0.1)
+                  , ((input$Food * 0.5)+(input$Atmosphere * 0.2)+(input$Character * 0.2)+(input$Service * 0.1))
+                  , (input$Atmosphere * 0.2)+(input$Character * 0.2)+(input$Service * 0.1)
+                  , format(input$Date, "%b - %Y")))
   })
   
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
+  
+  #write data back to folder
+  observeEvent(input$submit, {
+    write.table(data(), file="/home/ubuntu/appdata/restaurants.csv", sep=",", col.names= FALSE, row.names=F)
   })
   
-  output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-    
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  
+  #Create datatable to be displayed
+  
+  datatable<-eventReactive(c(input$agent,input$submit), {         
+    data.frame(read.table("/home/ubuntu/appdata/restaurants.csv",sep=",",header=T))
   })
   
+  
+  #Create table
+  output$summary <- renderTable({ datatable() }, digits=2,align = "c" )
+  
+  #Reset form
+  observeEvent(input$submit, {
+    reset("form")
+  })
+  
+  
+  #some text
+  output$selected_var <- renderText({
+    paste("Reenoel Metric: ",input$metric)
+  })
+  
+  #Create bar chart
+  output$barPlot<-renderPlotly({
+    plot_ly(({ datatable() }),
+            x = ({ datatable() })[,input$metric],
+            y = ~reorder(({ datatable() })$Restaurant
+                         , ({ datatable() })[,input$metric]),
+            type = 'bar', orientation = 'h',
+            hoverinfo = "text",
+            text = (paste(({ datatable() })$Restaurant, "<br>",
+                          paste(input$metric,"= ", ({ datatable() })[,input$metric]), "<br>",
+                          "Date: ", ({ datatable() })$Date, "<br>",
+                          "Time: ", ({ datatable() })$Meal)),
+            color = ({ datatable() })[,input$color_change]
+            #({ datatable() })[,input$color_change]
+    ) %>%
+      layout(margin = list(l = 180, r = 50, b = 90, t = 30, pad = 4),
+             width = 800, height = 620,
+             yaxis = list(title = "",showgrid = FALSE, showline = FALSE, showticklabels = TRUE),
+             xaxis = list(zeroline = FALSE, showline = FALSE, showticklabels = FALSE, showgrid = TRUE),
+             showlegend= FALSE,
+             paper_bgcolor = 'rgb(248, 248, 255)',
+             plot_bgcolor = 'rgb(248, 248, 255)') %>%
+      add_annotations(xref = 'x', yref = 'y',
+                      x = ({ datatable() })[,input$metric] + 0.5,
+                      text = round(({ datatable() })[,input$metric], 1),
+                      font = list(family = 'Arial', size = 15),
+                      showarrow = FALSE) %>%
+      add_annotations(xref = 'paper', yref = 'paper',
+                      x = -0.20, y = -0.07,
+                      text = paste('Created to celebrate the 34th birthday of Reena Chieng'),
+                      font = list(family = 'Arial', size = 10, color = 'rgb(150,150,150)'),
+                      showarrow = FALSE)
+  })
 }
 
-# Run the application 
+
+# Run the application
 shinyApp(ui = ui, server = server)
 
